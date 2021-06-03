140727254a00c6b930bf8047a6e719acd827de4e
Beervanu <62481668+Beervanu@users.noreply.github.com>
2021-06-03 13:32:49 +0100

    Scripted commit

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 //not mine
+require('dotenv').config()
 const path = require('path')
 const Discord = require('discord.js')
 
 //mine
-const si = require('./sensitiveInfo.json')
 const command = require('./CommandHandler/command_handler.js')
 
 const client = new Discord.Client()
 // '807914451457146900' cockslayer id
-const ch = new command.CommandHandler(client, '.', 'Hope', path.resolve('./Guilds'), si.backup_id)
+const ch = new command.CommandHandler(client, '.', 'Hope', path.resolve('./Guilds'), process.env.BACKUP_ID)
 ch.add_commands(path.resolve('./Commands'), path.resolve('./Checks'))
 ch.add_events(path.resolve('./Events'))
 ch.template=path.resolve('./template.json')
 
-client.login(si.tokens.Hope) //actual bot
-// client.login(si.tokens.Test986) // test
+client.login(process.env.TOKEN) //actual bot
+// client.login(process.env.TEST) // test
