5bf0adfa250237d9003f3b70f378df64c0189499
Beervanu <62481668+Beervanu@users.noreply.github.com>
2021-06-28 19:56:59 +0100

    Scripted commit

--- Commands/kill.js
+++ Commands/kill.js
@@ -2,22 +2,14 @@ const Heroku = require('heroku-client')
 
 exports.func = async function kill(msg, parameters)
 {
-	if(process.env.HEROKU_API_KEY)
-	{
-		let app_names = ['hope-js', 'hope-js2']
-		let hk = new Heroku({token: process.env.HEROKU_API_KEY})
-		hk.post(`/apps/${app_names[process.env.APP_VERSION]}/dynos/${process.env.dyno}/actions/stop`)
-	}
-	else 
-	{
-		let signal = parameters[0] || "SIGTERM"
-		process.emit(signal)
-	}
+	let app_names = ['hope-js', 'hope-js2']
+	let hk = new Heroku({token: process.env.HEROKU_API_KEY})
+	hk.post(`/apps/${app_names[process.env.APP_VERSION]}/dynos/${process.env.dyno}/actions/stop`)
 }
 
 exports.info = {
 	aliases: ['k', 'stop'],
 	group: 'Dev',
 	hidden: true,
-	checks: ['only_me']
+	checks: ['only_me', 'on_heroku']
 }
