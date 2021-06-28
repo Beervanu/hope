a57fe75e1689aec6ec9c33be0b8dc835a801900a
Beervanu <62481668+Beervanu@users.noreply.github.com>
2021-06-28 19:47:17 +0100

    Scripted commit

--- Commands/kill.js
+++ Commands/kill.js
@@ -1,7 +1,16 @@
 exports.func = async function kill(msg, parameters)
 {
-	let signal = parameters[0] || "SIGTERM"
-	process.emit(signal)
+	if(process.env.HEROKU_API_KEY)
+	{
+		let app_names = ['hope-js', 'hope-js2']
+		let hk = new Heroku({token: process.env.HEROKU_API_KEY})
+		hk.post(`/apps/${app_names[process.env.APP_VERSION]}/dynos/${process.env.dyno}/actions/stop`)
+	}
+	else 
+	{
+		let signal = parameters[0] || "SIGTERM"
+		process.emit(signal)
+	}
 }
 
 exports.info = {
--- Commands/move_hope.js
+++ Commands/move_hope.js
@@ -8,7 +8,7 @@ exports.func = async function move_hope(msg, parameters)
     	quantity: 1,
     	size: 'free'
     }})
-    hk.post(`/apps/${app_names[process.env.APP_VERSION]}/dynos/${process.env.DYNO}`)
+    hk.post(`/apps/${app_names[process.env.APP_VERSION]}/dynos/${process.env.dyno}/actions/stop`)
 }
 
 exports.info = {
@@ -18,5 +18,5 @@ exports.info = {
 	brief_desc: 'Moves the bot host',
 	long_desc: '[host_number(default:0)] - the number of the host (0-1)',
 	usage: '[host_number(default:0)]',
-	checks: ['only_me']
+	checks: ['only_me', 'on_heroku']
 }
