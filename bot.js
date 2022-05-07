63964a0253e80561bf46b3a59d2708a19971b6a2
Be'ervanu <fialhoj986@gmail.com>
2022-05-08 00:18:07 +0100

    add intents

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js')
 //mine
 const command = require('./CommandHandler/command_handler.js')
 
-const client = new Discord.Client()
+const client = new Discord.Client({intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES']})
 
 const ch = new command.CommandHandler(client, '.', 'Hope', path.resolve('./Guilds'), process.env.BACKUP_ID)
 client.login(process.env.TOKEN)
